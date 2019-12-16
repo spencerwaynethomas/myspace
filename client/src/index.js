7b0fc65ba240529d58a21cc92c2db8f9ac14b96e
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2019-12-16 15:19:44 -0700

    WIP

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, } from 'react-router-dom';
+import { AuthProvider, } from "./providers/AuthProvider";
+import { initMiddleware, } from 'devise-axios';
+import 'semantic-ui-css/semantic.min.css';
+
+
+initMiddleware();
+ReactDOM.render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
+  document.getElementById('root')
+);
+
+// registerServiceWorker();
